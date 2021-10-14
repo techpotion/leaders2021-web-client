9baa9b15c5bb32c18976125c2fa460de0063b383
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-10-14 18:44:11 +0300

    #9 generate map component

---
+++ src/map/map.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapComponent } from './components/map/map.component';
+
+
+
+@NgModule({
+  declarations: [
+    MapComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MapModule { }
