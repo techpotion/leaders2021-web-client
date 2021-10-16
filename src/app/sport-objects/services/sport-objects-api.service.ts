448b9a40c86ac9bea1ddc1eb645f6d2d4b6bc4f8
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-10-16 20:06:48 +0300

    #13 get data from backend

---
+++ src/app/sport-objects/services/sport-objects-api.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SportObjectsApiService {
+
+  constructor(
+    private readonly http: HttpClient,
+  ) { }
+
+  public getDensity(): Observable<GeoJSON.FeatureCollection> {
+    return this.http.get<{ geoJson: string }>('/GetGeoJsonObjects').pipe(
+      map(dto => JSON.parse(dto.geoJson) as GeoJSON.FeatureCollection),
+    );
+  }
+
+}
