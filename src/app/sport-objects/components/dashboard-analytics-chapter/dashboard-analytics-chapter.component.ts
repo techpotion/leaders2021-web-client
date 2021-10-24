71a38fcba81f28ee43c6c332e4a6b31fb4bd5b5b
Alexander Kostarev <kostarev.a.p@gmail.com>
2021-10-24 20:54:53 +0300

    #28 partly make 3rd section of dashboard

--- src/app/sport-objects/components/dashboard-analytics-chapter/dashboard-analytics-chapter.component.ts
+++ src/app/sport-objects/components/dashboard-analytics-chapter/dashboard-analytics-chapter.component.ts
@@ -1,4 +1,16 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+} from '@angular/core';
+
+import { BehaviorSubject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+import { isNotNil } from '../../../shared/utils/is-not-nil';
+
+import { FullPolygonAnalytics } from '../../models/polygon-sport-analytics';
+
 
 @Component({
   selector: 'tp-dashboard-analytics-chapter',
@@ -9,5 +21,28 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 export class DashboardAnalyticsChapterComponent {
 
   constructor() { }
+
+  public readonly analyticsSubject =
+  new BehaviorSubject<FullPolygonAnalytics | null>(null);
+
+  @Input()
+  public set analytics(value: FullPolygonAnalytics | null) {
+    this.analyticsSubject.next(value);
+  }
+
+  public readonly areasCount = this.analyticsSubject.pipe(
+    filter(isNotNil),
+    map(analytics => analytics.basicAnalytics.areasAmount),
+  );
+
+  public readonly sportKindsCount = this.analyticsSubject.pipe(
+    filter(isNotNil),
+    map(analytics => analytics.basicAnalytics.sportsAmount),
+  );
+
+  public readonly areasSquare = this.analyticsSubject.pipe(
+    filter(isNotNil),
+    map(analytics => Math.floor(analytics.basicAnalytics.areasSquare)),
+  );
 
 }
